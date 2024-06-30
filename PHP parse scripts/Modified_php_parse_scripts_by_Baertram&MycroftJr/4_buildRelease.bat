a9e8d5c7da8b4f5c8564e4723993297425304bc6
Baertram <pz1@gmx.net>
2024-06-30 21:20:29 +0200

    Updated batch files

--- PHP parse scripts/Modified_php_parse_scripts_by_Baertram&MycroftJr/4_buildRelease.bat
+++ PHP parse scripts/Modified_php_parse_scripts_by_Baertram&MycroftJr/4_buildRelease.bat
@@ -8,8 +8,6 @@ echo.
     xcopy "_out\xml\*.json" "_release/xml/*.*" /y
     xcopy "manual_update\*.lua" "_release/*.*" /y
 echo ---------------------------------------------------------------------------
-echo !Please change the APIVersion at the bottom of file \_release\xml\index.html (search for 'for API level') to the current one (use /script d(GetAPIVersion() ingame)
-echo ---------------------------------------------------------------------------
 echo.
 echo -Finished-
 pause
