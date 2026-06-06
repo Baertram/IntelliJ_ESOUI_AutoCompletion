f4458212cc01cb7e6d3b49fe977fef952e29780f
Baertram <1195208+Baertram@users.noreply.github.com>
2026-06-06 21:59:20 +0200

    Trying download from UESP, then manual dl file parse
    Trying download from UESP current APIversion and 2 last ones and if cloudflare blocks that, then use the manually downloaded globals.txt from folder "_out/_noRelease/globals.txt"

--- PHP parse scripts/Modified_php_parse_scripts_by_Baertram&MycroftJr/parser_scripts/global_vars.php
+++ PHP parse scripts/Modified_php_parse_scripts_by_Baertram&MycroftJr/parser_scripts/global_vars.php
@@ -2,55 +2,88 @@
 /*
 	Parse esoui Documentation and generates lua table 
 	for exporting ingame global values for IDE helpers.
+	
+	IMPORTANT: If Cloudflare is protecting the download of the Globals.txt (the run script shows it you as output information!), 
+	you need to manually load this file, or if it does not exist any older file (APIVersion - 1):
+	"https://esoapi.uesp.net/<APIVersionHere e.g. 101049>/globals.txt") -> "https://esoapi.uesp.net/101049/globals.txt"
+	And put the downloaded globals.txt file into this folder:
+	-> See variable $cachedFile below, usual folder would be "_out/_noRelease/globals.txt"
 */
 
 include_once dirname(__FILE__)."/esouiAPIDoc.php";
 
-function UR_exists($url){
-   $headers=get_headers($url);
-   return stripos($headers[0],"200 OK")?true:false;
+$cachedFile = "_out/_noRelease/globals.txt";
+
+function fetch_url_contents($url) {
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_TIMEOUT        => 30,
+        CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+		//CURLOPT_CAINFO         => 'C:/xampp/cacert.pem',  // <-- adjust path to where you saved the CURL certificate to, if not added globally to your php.ini
+    ]);
+    $content = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return $httpCode === 200 ? $content : false;
 }
 
 class global_vars
 {
     public function __construct()
     {
-        global $esoui_API_doc_filename;
+        global $esoui_API_doc_filename, $cachedFile;
         $array = file($esoui_API_doc_filename, FILE_IGNORE_NEW_LINES);
         [$classes, $apiVersion] = $this->parseClasses($array);
         $globals = $classes['Globals'];
         unset($classes['Globals']);
-        
-		$apiVersionBefore = $apiVersion;
-        // Try to download globals.txt to read items from -> Current APIVersion
-		if ( UR_exists("https://esoapi.uesp.net/$apiVersion/globals.txt") ) {
-			$UESP_globalsOfAPIVersion = fopen("https://esoapi.uesp.net/$apiVersion/globals.txt", 'r');
-		} else {
-			// Because UESP globals.txt might take weeks to update we try to use the last available APIversion's globals.txt
-			$apiVersionBefore = strval(intval($apiVersion) - 1);
-			print '[ERROR]New APIVersion globals.txt not found at UESP. Using version ' . $apiVersionBefore;
-			if ( UR_exists("https://esoapi.uesp.net/$apiVersionBefore/globals.txt") ) {
-				$UESP_globalsOfAPIVersion = fopen("https://esoapi.uesp.net/$apiVersionBefore/globals.txt", 'r');
-			} else {
-				print '[ERROR]New APIVersion globals.txt not found at UESP. Using version ' . $apiVersionBefore;
-				$apiVersionBefore = strval(intval($apiVersion) - 2);
-				if ( UR_exists("https://esoapi.uesp.net/$apiVersionBefore/globals.txt") ) {
-					$UESP_globalsOfAPIVersion = fopen("https://esoapi.uesp.net/$apiVersionBefore/globals.txt", 'r');
-				} else {
-					print '[ERROR]APIVersion globals.txt not found at UESP!';
-				}
-			}
-		}	
-		
-		if ( isset($UESP_globalsOfAPIVersion) ) {
-			if (file_put_contents("_out/_noRelease/globals.txt", $UESP_globalsOfAPIVersion)) {
-				// Parse custom enums out of globals.txt using the names and patterns in customEnums.txt
-				$glob = file("_out/_noRelease/globals.txt", FILE_IGNORE_NEW_LINES);
-				$enums = file("manual_update/customEnums.txt", FILE_IGNORE_NEW_LINES);
-				$customEnums = $this->parseEnums($enums, $glob);
-			}
-		}
-        
+	
+        $versionsToTry = [
+            $apiVersion,
+            strval(intval($apiVersion) - 1),
+            strval(intval($apiVersion) - 2),
+        ];
+
+        $UESP_globalsOfAPIVersion = false;
+
+        // First try to fetch from UESP online
+        foreach ($versionsToTry as $version) {
+            $content = fetch_url_contents("https://esoapi.uesp.net/$version/globals.txt");
+            if ($content !== false) {
+                $UESP_globalsOfAPIVersion = $content;
+                if ($version !== $apiVersion) {
+                    print '[INFO] Trying globals.txt from older API version ' . $version . PHP_EOL;
+                }
+                break;
+            } else {
+                print '[ERROR] globals.txt not found at UESP for API version ' . $version . PHP_EOL;
+            }
+        }
+
+        // If all online attempts failed, fall back to the last locally cached copy
+        if ($UESP_globalsOfAPIVersion === false) {
+            if (file_exists($cachedFile)) {
+                print '[WARNING] Cloudflare blocked all requests. Falling back to locally cached globals.txt' . PHP_EOL;
+                $UESP_globalsOfAPIVersion = file_get_contents($cachedFile);
+            } else {
+                print '[ERROR] No globals.txt found online or locally. Please download it manually from:' . PHP_EOL;
+                print '        https://esoapi.uesp.net/' . $apiVersion . '/globals.txt' . PHP_EOL;
+                print '        and save it to: ' . $cachedFile . PHP_EOL . PHP_EOL;
+            }
+        }
+
+        if ( $UESP_globalsOfAPIVersion !== false ) {
+            if (file_put_contents("_out/_noRelease/globals.txt", $UESP_globalsOfAPIVersion)) {
+                // Parse custom enums out of globals.txt using the names and patterns in customEnums.txt
+                print 'Globals.txt found: Parsing enums ...' . PHP_EOL . PHP_EOL;
+                $glob = file("_out/_noRelease/globals.txt", FILE_IGNORE_NEW_LINES);
+                $enums = file("manual_update/customEnums.txt", FILE_IGNORE_NEW_LINES);
+                $customEnums = $this->parseEnums($enums, $glob);
+            }
+        }
+
         $out = "if DumpVars == nil then DumpVars = {} end\n\nDumpVars.enumsToDump = {\n";
 
         if ( isset($customEnums) ) {
@@ -70,11 +103,11 @@ class global_vars
             $out = substr($out, 0,-2)."\n},\n";
         }
         $out = substr($out, 0,-2)."\n}";
-        
+
         // Now list the non-enum constants to dump
         $out .= "\n\nDumpVars.constantsToDump = {\n";
         $constants = file("manual_update/customConstants.txt", FILE_IGNORE_NEW_LINES);
-         if ( isset($constants) ) {
+        if ( isset($constants) ) {
             foreach ($constants as $var) {
                 if ($var != "") $out .= "\t['$var'] = $var,\n";
             }
@@ -122,7 +155,7 @@ class global_vars
 
                             // While we still have strings to compare,
                             // if the indexed character is the same in both strings,
-                            // increment the index. 
+                            // increment the index.
                             for ($i=0; $i<$len && $s1[$i] == $s2[$i]; $i++);
 
                             $prefix = substr($s1, 0, $i);
@@ -140,7 +173,7 @@ class global_vars
                             }
                         }
                     }
-                    
+
                     // Then start the new section
                     $tag = $matches['section'];
                 } else if ($tag) {
@@ -154,7 +187,7 @@ class global_vars
         }
         return [$objects, $apiVersion];
     }
-    
+
     function parseEnums($enumTxt, $global) {
         if ( isset($enumTxt) ) {
             $patterns = [];
